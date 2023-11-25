35c0fc479da639c699ccb0ec61559f7ade488393
fadedreams7 <fadedreams7@gmail.com>
2023-11-25 12:17:26 -0500

    pgx&godotenv+

---
+++ postgres/postgres.go
@@ -0,0 +1,44 @@
+package postgres
+
+import (
+	"context"
+	"log"
+
+	"github.com/fadedreams/xclone/config"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type DB struct {
+	Pool *pgxpool.Pool
+}
+
+func New(ctx context.Context, conf *config.Config) *DB {
+	// dbConf, err := pgxpool.ParseConfig("postgres://postgres:postgres@localhost:5432/postgres")
+	dbConf, err := pgxpool.ParseConfig(conf.Database.URL)
+	if err != nil {
+		log.Fatalf("can't parse postgres config: %v", err)
+	}
+
+	pool, err := pgxpool.ConnectConfig(ctx, dbConf)
+	if err != nil {
+		log.Fatalf("error connecting to postgres: %v", err)
+	}
+
+	db := &DB{Pool: pool}
+
+	db.Ping(ctx)
+
+	return db
+}
+
+func (db *DB) Ping(ctx context.Context) {
+	if err := db.Pool.Ping(ctx); err != nil {
+		log.Fatalf("can't ping postgres: %v", err)
+	}
+
+	log.Println("postgres connected")
+}
+
+func (db *DB) Close() {
+	db.Pool.Close()
+}
